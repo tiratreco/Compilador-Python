85362fba7a2b567f253fef178b94cc8e373edb2d
Gustavo André <tiratreco@gmail.com>
2022-05-03 11:30:08 -0300

    Create JasminGenerator.py

---
+++ JasminGenerator.py
@@ -0,0 +1,20 @@
+
+
+class Generator():
+
+    def __init__(self, name):
+        self.name = name
+        # cria arquivo
+
+
+
+    def int_sum(self, reg1, reg2, result):
+        # salva no arquivo:
+        # """
+        # ; int_sum
+        # iload {}
+        # iload {}
+        # iadd
+        # istore {}
+        # """.format(reg1, reg2, result)
+        pass
