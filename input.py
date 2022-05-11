a6983ec69627d966354425ff74e7ad4371c89f24
Gustavo André <tiratreco@gmail.com>
2022-05-11 11:48:55 -0300

    Update input.py

--- input.py
+++ input.py
@@ -1,10 +1,12 @@
 int count, lim;
-
+boolean teste;
 
 main():
+    teste = input();
     lim = input();
     print lim;
     for count in range(lim):
         print count;
     }
+    print not teste;
 }
